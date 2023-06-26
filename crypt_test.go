455fe59845d7d34a5c9fef68856e901c3b2fd85e
Antonio <acheong@student.dalat.org>
2023-06-26 23:43:05 +0800

    simplify

--- crypt_test.go
+++
@@ -1,8 +0,0 @@
-package funcaptcha
-
-import "testing"
-
-func TestEncryptDecrypt1(t *testing.T) {
-	enc := Encrypt("test-data", "test-key")
-	println(enc)
-}
--- util_test.go
+++
@@ -1,8 +0,0 @@
-package funcaptcha
-
-import "testing"
-
-func TestGetBda(t *testing.T) {
-	res := GetBda("test-useragent", "test-referer", "test-location")
-	println(res)
-}
