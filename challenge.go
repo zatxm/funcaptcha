757411805bdeeff69dbc8bd9e6288d388b3a7f04
Antonio <acheong@student.dalat.org>
2023-06-30 17:25:40 +0800

    logger

--- challenge.go
+++ challenge.go
@@ -29,37 +29,46 @@ var headers = http.Header{
 }
 
 type Session struct {
-	Sid          string `json:"sid"`
-	SessionToken string `json:"session_token"`
-	Hex          string `json:"hex"`
+	Sid             string
+	SessionToken    string
+	Hex             string
+	ChallengeLogger challengeLogger
 }
 
 type Challenge struct {
-	SessionToken         string                 `json:"session_token"`
-	ChallengeID          string                 `json:"challengeID"`
-	ChallengeURL         string                 `json:"challengeURL"`
-	AudioChallengeURLs   []string               `json:"audio_challenge_urls"`
-	AudioGameRateLimited interface{}            `json:"audio_game_rate_limited"`
-	Sec                  int                    `json:"sec"`
-	EndURL               interface{}            `json:"end_url"`
-	GameData             map[string]interface{} `json:"game_data"`
-	GameSID              string                 `json:"game_sid"`
-	SID                  string                 `json:"sid"`
-	Lang                 string                 `json:"lang"`
-	StringTablePrefixes  []interface{}          `json:"string_table_prefixes"`
-	StringTable          map[string]string      `json:"string_table"`
-	EarlyVictoryMessage  interface{}            `json:"earlyVictoryMessage"`
-	FontSizeAdjustments  interface{}            `json:"font_size_adjustments"`
-	StyleTheme           string                 `json:"style_theme"`
+	SessionToken         string      `json:"session_token"`
+	ChallengeID          string      `json:"challengeID"`
+	ChallengeURL         string      `json:"challengeURL"`
+	AudioChallengeURLs   []string    `json:"audio_challenge_urls"`
+	AudioGameRateLimited interface{} `json:"audio_game_rate_limited"`
+	Sec                  int         `json:"sec"`
+	EndURL               interface{} `json:"end_url"`
+	GameData             struct {
+		GameType  int `json:"gameType"`
+		CustomGUI struct {
+			ChallengeIMGs []string `json:"_challenge_imgs"`
+		} `json:"customGUI"`
+	} `json:"game_data"`
+	GameSID             string            `json:"game_sid"`
+	SID                 string            `json:"sid"`
+	Lang                string            `json:"lang"`
+	StringTablePrefixes []interface{}     `json:"string_table_prefixes"`
+	StringTable         map[string]string `json:"string_table"`
+	EarlyVictoryMessage interface{}       `json:"earlyVictoryMessage"`
+	FontSizeAdjustments interface{}       `json:"font_size_adjustments"`
+	StyleTheme          string            `json:"style_theme"`
 }
 
-type initialChallenge struct {
+type challengeLogger struct {
 	Sid           string `json:"sid"`
 	SessionToken  string `json:"session_token"`
 	AnalyticsTier int    `json:"analytics_tier"`
 	RenderType    string `json:"render_type"`
 	Category      string `json:"category"`
 	Action        string `json:"action"`
+	// Omit if empty
+	GameToken string `json:"game_token,omitempty"`
+	GameType  string `json:"game_type,omitempty"`
 }
 
 type requestChallenge struct {
@@ -77,35 +86,20 @@ func StartChallenge(full_session, hex string) (*Session, error) {
 	fields := strings.Split(full_session, "|")
 	session_token := fields[0]
 	sid := strings.Split(fields[1], "=")[1]
-	// Convert challenge to url.Values
-	v := initialChallenge{
+
+	session := Session{
+		Sid:          sid,
+		SessionToken: session_token,
+		Hex:          hex,
+	}
+	session.ChallengeLogger = challengeLogger{
 		Sid:           sid,
 		SessionToken:  session_token,
 		AnalyticsTier: 40,
 		RenderType:    "canvas",
-		Category:      "Site URL",
-		Action:        fmt.Sprintf("https://tcr9i.chat.openai.com/v2/1.5.2/enforcement.%s.html", hex),
 	}
-	request, _ := http.NewRequest(http.MethodPost, "https://tcr9i.chat.openai.com/fc/a/", strings.NewReader(jsonToForm(toJSON(v))))
-	request.Header = headers
-	resp, err := (*client).Do(request)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("status code %d", resp.StatusCode)
-	}
-
-	body, _ := io.ReadAll(resp.Body)
-	log.Println(string(body))
-
-	return &Session{
-		Sid:          sid,
-		SessionToken: session_token,
-		Hex:          hex,
-	}, nil
+	err := session.log("", 0, "Site URL", fmt.Sprintf("https://tcr9i.chat.openai.com/v2/1.5.2/enforcement.%s.html", hex))
+	return &session, err
 }
 
 func (c *Session) RequestChallenge() (*Challenge, error) {
@@ -136,6 +130,32 @@ func (c *Session) RequestChallenge() (*Challenge, error) {
 	log.Println(string(body))
 	var challenge_data Challenge
 	err = json.Unmarshal(body, &challenge_data)
+	if err != nil {
+		return nil, err
+	}
+	err = c.log(challenge_data.ChallengeID, challenge_data.GameData.GameType, "loaded", "game loaded")
 	return &challenge_data, err
+}
 
+func (c *Session) log(game_token string, game_type int, category, action string) error {
+	v := c.ChallengeLogger
+	v.GameToken = game_token
+	if game_type != 0 {
+		v.GameType = fmt.Sprintf("%d", game_type)
+	}
+	v.Category = category
+	v.Action = action
+
+	request, _ := http.NewRequest(http.MethodPost, "https://tcr9i.chat.openai.com/fc/a/", strings.NewReader(jsonToForm(toJSON(v))))
+	request.Header = headers
+	resp, err := (*client).Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("status code %d", resp.StatusCode)
+	}
+	return nil
 }
